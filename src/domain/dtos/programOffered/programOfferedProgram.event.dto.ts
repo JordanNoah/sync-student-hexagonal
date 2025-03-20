dc41aeb9fdbf75080f1f367c0db3d275ec48a10b
jordan.ubilla <jordan.ubilla@funiber.com>
2025-03-20 14:03:15 -0500

    update group names

--- src/domain/dtos/programOffered/programOfferedProgram.event.dto.ts
+++ src/domain/dtos/programOffered/programOfferedProgram.event.dto.ts
@@ -9,7 +9,6 @@ export default class ProgramOfferedProgramEventDto {
         const {uuid, abbreviation, version} = object
         const messageErrorComplement = 'missing in programOfferedProgram structure'
         if (!uuid) return [`uuid ${messageErrorComplement}`, undefined]
-        if (!version) return [`version ${messageErrorComplement}`, undefined]
         if (!abbreviation) return [`abbreviation ${messageErrorComplement}`, undefined]
 
         return [undefined, new ProgramOfferedProgramEventDto(uuid, version, abbreviation)]
