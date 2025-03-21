e9957d85bf166188a1073abc2092ce31a274c0f4
Hector Arismendi <hector.arismendi@funiber.org>
2025-03-21 11:01:14 -0500

    Feature: Deleted .env and dist.

--- dist/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = require("./presentation/server");
-(() => {
-    main();
-})();
-function main() {
-    new server_1.Server({}).start();
-}
